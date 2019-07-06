63ea299d3eee4c86809b39477553ff34a80e4fcf
Patrick Haberern <46791449+Habernet@users.noreply.github.com>
2019-07-05 23:52:29 -0400

    Second to last push to heroku (#84)
    * update
    
    * updating map
    
    * this is a update
    
    * update
    
    * closed a tag (#68)
    
    * Master (#70)
    
    * hot fix
    
    * Fixed for real this time (#38)
    
    * closed a tag (#68) (#69)
    
    * Changed tables to Tables (#72)
    
    * fixed button issue
    
    * Edited functionality,added/deleted buttons
    
    * updated image and css
    
    * Added slick.js carousel for reviews
    
    * added shadows where I could
    
    * styled the index page
    
    * shadow added to buttons,box

--- public/js/map.js
+++ public/js/map.js
@@ -16,3 +16,17 @@ L.tileLayer(
 ).addTo(mymap);
 // eslint-disable-next-line no-unused-vars
 var marker = L.marker([35.85504815, -78.8406595376177]).addTo(mymap);
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = $(".mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
